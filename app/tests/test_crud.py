312fe7e4c2d4d79d8be42349a18a556237d19487
stavtasa <stavtasa@gmail.com>
2023-07-25 18:57:47 +0300

    add const message path & add asserts for errors

--- app/tests/test_crud.py
+++ app/tests/test_crud.py
@@ -2,6 +2,8 @@ import pytest
 from app import app, db
 from DB.models import Message
 from DB.db_handler import *
+from routes import MESSAGE_PATH
+import json 
 
 
 @pytest.fixture(scope="module")
@@ -11,10 +13,9 @@ def test_client():
 
     with flask_app.test_client() as testing_message:
         with flask_app.app_context():
-            create_test_messages()
+            create_test_message()
             yield testing_message
             delete_test_messages("test")
-
 
 
     #### POSITIVE TESTS ####
@@ -30,61 +31,59 @@ def test_create_messages(test_client):
         "message_title":"test_create_message"}
 
     
-    response = test_client.post('/message', json=message)
+    response = test_client.post(f'/{MESSAGE_PATH}', json=message)
     delete_test_messages("test_create_message")
     assert response.status_code == 201
     
 
 def test_get_message(test_client):
-    create_test_messages()
-    message = get_message_by_title("test")
-    response = test_client.get(f'/message/{message.id}')
+    message = create_test_message()
+    response = test_client.get(f'/{MESSAGE_PATH}/{message.id}')
+    delete_test_messages("test")
     assert response.status_code == 200
 
 
 def test_update_message(test_client):
     new_message = {
-        "user_id":"1",
-        "repeat":"daily",
-        "dest_groups_id":"1, 2, 3",
-        "time_to_send":"2023-10-10, 10:10:10",
-        "message_data":"test",
-        "message_title":"test_update_message"}
-    create_test_messages()
-    message = get_message_by_title("test")
-    response = test_client.put(f'/message/{message.id}', json=new_message)
-    delete_test_messages("test_update_message")
+        "message_data":"this field changed"
+       }
+
+    message = create_test_message()
+    response = test_client.put(f'/{MESSAGE_PATH}/{message.id}', json=new_message)
+    id = Message.query.filter_by(message_data="this field changed").first().id
+    delete_test_messages("test")
     assert response.status_code == 200
+    assert  id == message.id
 
 
 def test_delete_message(test_client):
-    create_test_messages()
-    message = get_message_by_title("test")
-    response = test_client.delete(f'/message/{message.id}')
+    message = create_test_message()
+    response = test_client.delete(f'/{MESSAGE_PATH}/{message.id}')
     assert response.status_code == 204
 
 
    #### NEGATIVE TESTS ####
 
 def test_get_non_existing_message(test_client):
-    response = test_client.get('/message/-1')
+    response = test_client.get(f'/{MESSAGE_PATH}/0', json={})
     assert response.status_code == 404
+    assert json.loads(response.data)['error'] == ERROR_MESSAGE_NOT_FOUND
 
 
 def test_update_non_existing_message(test_client):
-    response = test_client.put('/message/-1', json="")
+    response = test_client.put(f'/{MESSAGE_PATH}/0', json={})
     assert response.status_code == 404
+    assert json.loads(response.data)['error'] == ERROR_MESSAGE_NOT_FOUND
 
 
 def test_delete_non_existing_message(test_client):
-    response = test_client.delete('/message/-1')
+    response = test_client.delete(f'/{MESSAGE_PATH}/0', json={})
     assert response.status_code == 404
+    assert json.loads(response.data)['error'] == ERROR_MESSAGE_NOT_FOUND
+
 
 
 #####
-def get_message_by_title(title):
-    message = Message.query.filter_by(message_title=title).first()
-    return message
 
 
 def delete_test_messages(title):
@@ -92,7 +91,7 @@ def delete_test_messages(title):
     db.session.commit()
 
 
-def create_test_messages(user_id = "1",
+def create_test_message(user_id = "1",
                         repeat = "daily",
                         dest_groups_id = "1, 2, 3",
                         time_to_send = "2023-10-10, 10:10:10",
@@ -108,6 +107,7 @@ def create_test_messages(user_id = "1",
                             message_title = message_title)
     db.session.add(new_message)
     db.session.commit()
+    return new_message
 
 
         
